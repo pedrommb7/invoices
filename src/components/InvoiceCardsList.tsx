b3f075c5d1ec328127a4b79b1e130aaf27a019ae
Pedro Barbosa <nb27942@novabase.pt>
2022-07-08 09:52:07 +0100

    Dummy data

--- src/components/InvoiceCardsList.tsx
+++ src/components/InvoiceCardsList.tsx
@@ -4,11 +4,11 @@ import InvoiceCard from './InvoiceCard';
 const InvoiceCardsList = () => {
   return (
     <>
-        <InvoiceCard data={{price: 20, name:"Name", date:"Due DD-MM-YYYY", state:"paid", ID:"INV_X", profession:"Graphic Design"}}/>
-        <InvoiceCard data={{price: 20, name:"Name", date:"Due DD-MM-YYYY", state:"paid", ID:"INV_X", profession:"Graphic Design"}}/>
-        <InvoiceCard data={{price: 20, name:"Name", date:"Due DD-MM-YYYY", state:"paid", ID:"INV_X", profession:"Graphic Design"}}/>
-        <InvoiceCard data={{price: 20, name:"Name", date:"Due DD-MM-YYYY", state:"paid", ID:"INV_X", profession:"Graphic Design"}}/>
-        <InvoiceCard data={{price: 20, name:"Name", date:"Due DD-MM-YYYY", state:"paid", ID:"INV_X", profession:"Graphic Design"}}/>
+        <InvoiceCard data={{price: 200, name:"John Stephen", date:"Due DD-MM-YYYY", state:"Paid", ID:"INV_X", profession:"Graphic Design"}}/>
+        <InvoiceCard data={{price: 200, name:"Name", date:"Due DD-MM-YYYY", state:"Paid", ID:"INV_X", profession:"Graphic Design"}}/>
+        <InvoiceCard data={{price: 200, name:"Name", date:"Due DD-MM-YYYY", state:"Paid", ID:"INV_X", profession:"Graphic Design"}}/>
+        <InvoiceCard data={{price: 200, name:"Name", date:"Due DD-MM-YYYY", state:"Paid", ID:"INV_X", profession:"Graphic Design"}}/>
+        <InvoiceCard data={{price: 200, name:"Name", date:"Due DD-MM-YYYY", state:"Paid", ID:"INV_X", profession:"Graphic Design"}}/>
     </>
   )
 }
